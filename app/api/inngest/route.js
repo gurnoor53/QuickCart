6c48a5edc8699eafe2a0e751e9790641210a866f
gurnoor53 <gurnooronweb@gmail.com>
2025-04-23 23:26:12 +0530

    inngest

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../config/inngest";
-import { syncUserCreation, syncUserUpdate, syncUserDeletion } from "../../../config/inngest";
-export const { serve } = createServeHandler();
-// Create an API that serves zero functions
+import { inngest } from "@/config/inngest";
+import {
+  syncUserCreation,
+  syncUserUpdate,
+  syncUserDeletion,
+} from "@/config/inngest/functions";
+
+// Serve the functions with Inngest
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
